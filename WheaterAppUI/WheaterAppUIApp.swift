8b404a3b514fe616f0d2064f5d5418156aea8526
RobertoAbad995 <roberto.ramirez@enhanceit.us>
2022-08-01 09:12:42 -0400

    Initial Commit

---
+++ WheaterAppUI/WheaterAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheaterAppUIApp.swift
+//  WheaterAppUI
+//
+//  Created by Consultant on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WheaterAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
